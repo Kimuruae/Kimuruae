e824b24f9a2905dd75bb671db917976ccc4f6e5e
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-11-05 18:05:08 +0300

    Major changes made on my personal website to include: bootsrap,php, and Js

---
+++ blog.php
@@ -0,0 +1,12 @@
+<?php include_once("templates/head.php");?>
+<?php include_once("templates/nav.php");?>
+<body>
+  <header>
+    <h1>Blog</h1>
+    <p>Read my latest thoughts on tech, business, and life:</p>
+    <a href="index.html">Back to Home</a>
+  </header>
+  <section class="blog-grid">
+    <!-- List of blog posts with title and summary -->
+  </section>
+  <?php include_once("templates/footer.php");?> 
---
+++ portfolio.php
@@ -0,0 +1,11 @@
+<?php include_once("templates/head.php");?>
+<?php include_once("templates/nav.php");?>
+  <header>
+    <h1>Portfolio</h1>
+    <p>Here are some of the projects I've worked on:</p>
+    <a href="index.html">Back to Home</a>
+  </header>
+  <section class="project-grid">
+    <!-- Add detailed project cards here -->
+  </section>
+  <?php include_once("templates/footer.php");?> 
